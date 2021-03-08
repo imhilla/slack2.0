dac96fcdcb8626ae37763a43f292226b3e6b6925
imhilla <hillaryodhiambo@gmail.com>
2021-03-08 14:48:51 +0300

    finalize header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon  from '@material-ui/icons/HelpOutline';
 
 export default function Header() {
   return (
@@ -20,6 +21,10 @@ export default function Header() {
       </HeaderSearch>
 
       {/* Header Right */}
+
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   )
 }
@@ -28,18 +33,20 @@ const HeaderSearch = styled.div`
    flex:0.4;
    opacity: 1;
    border-radius: 6px;
-   background-color: #421f40;
+   background-color: #421f44;
    text-align: center;
+   display: flex;
    padding: 0 50px;
    color: gray;
    border: 1px gray solid;
-    > input {
-      background-color: transparent;
-      border: none;
-      text-align: center;
-      min-width: 30vw; 
-      outline: 0;
-      color: white;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw; 
+    outline: 0;
+    color: white;
     }
 `
 const HeaderContainer = styled.div`
@@ -69,3 +76,14 @@ cursor: pointer;
   opacity: 0.8;
 }
 `;
+
+const HeaderRight = styled.div`
+   flex:0.3;
+   display: flex;
+   align-items: flex-end;
+
+   > .MuiSvgIcon-root {
+      margin-left: auto;
+      margin-right: 20px;
+   }
+`

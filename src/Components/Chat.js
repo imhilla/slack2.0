84c4c339932414c9c46c90ec0029aa5e2cd03922
imhilla <hillaryodhiambo@gmail.com>
2021-03-08 17:50:40 +0300

    add chat left headers and styles

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,16 +1,53 @@
 import React from 'react';
+import { StarBorderOutlined, InfoOutlined } from '@material-ui/icons';
 import styled from 'styled-components';
 
 export default function Chat() {
   return (
     <ChatContainer>
-      <h1>I am the chat screen</h1>
+      <Header>
+        <HeaderLeft>
+          <h4><strong>#Room name</strong></h4>
+          <StarBorderOutlined />
+        </HeaderLeft>
+        <HeaderRight>
+          <p>
+            <InfoOutlined /> Details
+          </p>
+        </HeaderRight>
+      </Header>
     </ChatContainer>
   )
 }
+
+const Header = styled.div`
+  display:flex;
+  justify-content:space-between;
+  padding:20px;
+  border-bottom: 1px solid lightgray;
+`
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items:center;
+
+  > h4 {
+    display: flex;
+    align-items: center;
+    text-transform: lowercase;
+    margin-right: 10px;
+
+  > h4 .MuiSvgIcon-root {
+    margin-left:10px;
+    font-size: 18px;
+  }
+}
+`
+const HeaderRight = styled.div`
+`
 
 const ChatContainer = styled.div`
   flex: 0.7;
   flex-grow: 1;
   overflow-y: scroll;
+  margin-top: 60px;
 `

06fc6cd7786537838af6754a41d18061e5626dbd
imhilla <hillaryodhiambo@gmail.com>
2021-03-08 17:54:18 +0300

    header section complete

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -43,6 +43,16 @@ const HeaderLeft = styled.div`
 }
 `
 const HeaderRight = styled.div`
+  >p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+
+  >p .MuiSvgIcon-root {
+    margin-right: 5px !important;
+    font-size:16px;
+  }
 `
 
 const ChatContainer = styled.div`

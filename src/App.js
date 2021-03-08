8f69ff4dc612197829d4defb90c5010e2aac83d7
JB2001216 <ktigerdevelopment@gmail.com>
2021-03-08 11:25:22 +0300

    add project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+       <h1>Let's build slack</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAUUGYTn1gJ1Ozu-hIhzJ5iwAqZqpGKB2k",
+  authDomain: "slack-20b65.firebaseapp.com",
+  projectId: "slack-20b65",
+  storageBucket: "slack-20b65.appspot.com",
+  messagingSenderId: "690034360485",
+  appId: "1:690034360485:web:26fab921eb0076ff6f4d4c"
+};

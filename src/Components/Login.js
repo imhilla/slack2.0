d6463d5d58ae686e7e4bf6ebe894af8a1c082252
imhilla <hillaryodhiambo@gmail.com>
2021-03-09 12:35:42 +0300

    style login inner container

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -21,9 +21,14 @@ const LoginContainer = styled.div`
   place-items: center;
 `
 const LoginInnerContainer = styled.div`
->img {
-  object-fit: contain;
-  height: 100px;
-  margin-bottom: 40px;
-}
+  padding:100px;
+  text-align:center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  >img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
 `
